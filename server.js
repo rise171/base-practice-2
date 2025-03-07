b0f26e4f77d07801fc7cf6eafecf64857f954bd5
rise171 <krispetrova206@gmail.com>
2025-03-07 10:39:33 +0300

    remade server.js

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = 3000;
-
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Страница 404
-app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
-
-app.listen(PORT, () => {
-    console.log("Сервер запущен на http://localhost:${PORT}");
+const http = require('http'); 
+const fs = require('fs'); 
+const path = require('path'); 
+ 
+http.createServer((req, res) => { 
+    // Указываем путь к файлу 
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url); 
+ 
+    fs.readFile(filePath, 'utf8', (err, data) => { 
+        if (err) { 
+            // Если файл не найден, читаем страницу 404 
+            fs.readFile(path.join(__dirname, 'public/404.html'), 'utf8', (err404, data404) => { 
+                if (!err404) { 
+                    res.writeHead(404, { 'Content-Type': 'text/html' }); 
+                    res.end(data404); 
+                } else { 
+                    res.writeHead(500, { 'Content-Type': 'text/html' }); 
+                    res.end('<h1>500: Внутренняя ошибка сервера</h1>'); 
+                } 
+            }); 
+        } else { 
+            res.writeHead(200, { 'Content-Type': 'text/html' }); 
+            res.end(data); 
+        } 
+    }); 
+}).listen(8080, () => { 
+    console.log('Сервер запущен на порту 8080'); 
 });
